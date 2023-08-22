424a5b4441694ae097fbd250f94d6d4e63f3badc
black2code <alsdnr1080@gmail.com>
2023-08-22 20:37:56 +0900

    Add : 공통 response 추가

---
+++ src/main/java/com/example/econrich/global/dto/GlobalResponseCode.java
@@ -0,0 +1,14 @@
+package com.example.econrich.global.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+
+@Getter
+@RequiredArgsConstructor
+public enum GlobalResponseCode {
+    OK(200,"요청이 성공하였습니다.");
+
+    private final int code;
+    private final String message;
+}
