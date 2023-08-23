9d4b0c7e473e31d5dde0c186c177b24c1fe49a43
black2code <alsdnr1080@gmail.com>
2023-08-24 03:43:51 +0900

    Add : swagger config 구성

---
+++ src/main/java/com/example/econrich/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.example.econrich.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("코딩 과제")
+                .description("API 구현 과제")
+                .version("1.0.0");
+    }
+}
